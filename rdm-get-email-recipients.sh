c42f293586a0532babe46e0d16b39ff836869dbf
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-09-15 16:13:55 +0200

    Added example script to get recipients.csv file
    - by using the iRODS GenQuery

---
+++ rdm-get-email-recipients.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for e in $( iquest --no-page "%s,%s" "SELECT USER_NAME,META_USER_ATTR_VALUE WHERE USER_TYPE = 'rodsuser' AND META_USER_ATTR_NAME = 'email'" ); do
+    uid=$( echo $e | awk -F ',' '{print $1}')
+    email=$( echo $e | awk -F ',' '{print $2}')
+    uname=$( iquest "%s" "SELECT META_USER_ATTR_VALUE WHERE USER_NAME = '$uid' AND META_USER_ATTR_NAME = 'displayName'" )
+
+    echo ${email},${uname}
+done
